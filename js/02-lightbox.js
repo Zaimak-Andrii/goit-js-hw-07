23286208d9d94c6ee6bcba11ca9d62a527c81175
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2022-11-01 17:40:48 +0200

    Add second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('ul.gallery');
+
+//galleryRef.addEventListener('click', galleryClickHandler);
+
+let galleryMarkup = galleryItems
+  .map((item) => {
+    return `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+        
+      />
+    </a>
+  </li>`;
+  })
+  .join('');
+
+galleryRef.innerHTML = galleryMarkup;
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
